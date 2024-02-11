f585acc6a17efbb9d3fe27be7da26568dbdec705
BhupendraLawaniya <146478940+BhupendraLawaniya@users.noreply.github.com>
2024-02-11 11:37:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Registration from './Registration_Pages/Registration';
+
+function App() {
+  return (
+    <div >
+      <Registration/>
+    </div>
+  );
+}
+
+export default App;
