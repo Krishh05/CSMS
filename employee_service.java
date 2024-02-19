e2041558bcbf87b23d24f915de9654c8a4767b3f
Krishh05 <146420485+Krishh05@users.noreply.github.com>
2024-02-19 18:18:37 +0530

    Delete employee_service.java

--- employee_service.java
+++
@@ -1,9 +0,0 @@
-package com.example.registration.Service;
-
-import com.example.registration.Dto.EmployeeDTO;
-
-public interface employee_service {
-
-	String addEmployee(EmployeeDTO employeeDTO);
-
-}
