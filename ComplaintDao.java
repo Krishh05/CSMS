2d42084e8dca55ceb7fa8e4e029d5712b1c577a4
BhupendraLawaniya <146478940+BhupendraLawaniya@users.noreply.github.com>
2024-02-21 00:16:39 +0530

    Delete ComplaintDao.java

--- ComplaintDao.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.pojos.Complaint;
-
-public interface ComplaintDao extends JpaRepository<Complaint, Long> {
-
-}
